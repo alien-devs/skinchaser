ca0eb73171f579d9c4fc8b006ef6118ea4ce1623
Ivan Proskurnin <43935004+tragedyHub@users.noreply.github.com>
2021-10-24 01:04:51 +0300

    support page

---
+++ pages/support/index.js
@@ -0,0 +1,93 @@
+import MainLayout from "../../components/MainLayout";
+
+const Support = () => {
+	const supportItems = [
+		{
+			title: "Не могу пополнить счёт",
+			text: "Почему то выкидывает после редикта на платежку =(",
+			imgUrl: "/img/support/logo.png",
+		},
+		{
+			title: "Не могу пополнить счёт",
+			text: "Почему то выкидывает после редикта на платежку =(",
+			imgUrl: "/img/support/logo.png",
+		},
+		{
+			title: "Не могу пополнить счёт",
+			text: "Почему то выкидывает после редикта на платежку =(",
+			imgUrl: "/img/support/logo.png",
+		},
+		{
+			title: "Не могу пополнить счёт",
+			text: "Почему то выкидывает после редикта на платежку =(",
+			imgUrl: "/img/support/logo.png",
+		},
+		{
+			title: "Не могу пополнить счёт",
+			text: "Почему то выкидывает после редикта на платежку =(",
+			imgUrl: "/img/support/logo.png",
+		},
+		{
+			title: "Не могу пополнить счёт",
+			text: "Почему то выкидывает после редикта на платежку =(",
+			imgUrl: "/img/support/logo.png",
+		},
+		{
+			title: "Не могу пополнить счёт",
+			text: "Почему то выкидывает после редикта на платежку =(",
+			imgUrl: "/img/support/logo.png",
+		},
+		{
+			title: "Не могу пополнить счёт",
+			text: "Почему то выкидывает после редикта на платежку =(",
+			imgUrl: "/img/support/logo.png",
+		},
+	];
+
+	return (
+		<MainLayout>
+			<div className="support_page">
+				<div className="support_left">
+					<img src="/img/support/support_message.svg" alt="" />
+					<h2 className="support_left_title">Нужна помощь ?</h2>
+					<p className="support_left_text">
+						У вас остались вопросы без ответа?
+						<br /> У вас есть какие-нибудь предложения ?
+					</p>
+					{<div></div>}
+					<button className="support_left_btn">
+						ЗАДАТЬ ВОПРОС / ВАШИ ПРЕДЛОЖЕНИЯ
+					</button>
+				</div>
+				<div className="support_right">
+					{supportItems.map((item, key) => (
+						<div>
+							<div className="support_card">
+								<div className="support_card_left">
+									<img src={item.imgUrl} alt="" />
+									<div className="support_card_column">
+										<h2 className="support_card_theme">
+											{item.title}
+										</h2>
+										<h2 className="support_card_text">
+											{item.text}
+										</h2>
+									</div>
+								</div>
+								<div className="support_card_column">
+									<h2 className="support_card_state">
+										Новое сообщение <div></div>
+									</h2>
+									<h2 className="support_card_time">18:43</h2>
+								</div>
+							</div>
+                            <div className="support_line"></div>
+						</div>
+					))}
+				</div>
+			</div>
+		</MainLayout>
+	);
+};
+
+export default Support;
